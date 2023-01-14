d4aa74af1a19ac23092970d6f67d6bd5bee9678b
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2023-01-14 18:01:24 -0300

    adicionei um sobre mim

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -5,16 +5,15 @@ const AboutContainer = () => {
     <section className="about-container">
       <h2>Sobre</h2>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, dolores!
-        Repellendus eius consequuntur, voluptate deserunt perferendis mollitia
-        debitis est autem aperiam necessitatibus beatae assumenda illo rem
-        architecto quis dolor quae.
+        Me chamo Gabriel Coelho tenho 27 anos, e atualmente sou
+        estudante da Trybe.
+        Já estando próximo a minha formação,
+        busco por oportunidades de iniciar minha carreira como desenvolvedor, e adquirir ainda mais conhecimento em tecnologias diversas.
       </p>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos veritatis
-        aperiam, odit nihil placeat, dolorum sequi reprehenderit fugiat,
-        distinctio provident impedit modi neque sapiente voluptate voluptates.
-        Maiores sint at dolores?
+        O primeiro contato que tive com a tecnologia
+        foi durante o curso da Trybe,
+        onde descobri meu interesse por resolução de problemas e programação. A partir daí venho constantemente buscar aprendizado.
       </p>
     </section>
   );
