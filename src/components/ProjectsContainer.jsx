876aab8335476aade22dedc09ecea04e47234e42
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2023-01-14 19:22:42 -0300

    adicionei os projetos de fron

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
+
+
 const ProjectsContainer = () => {
   return (
     <section className="projects-container">
       <h2>Projetos</h2>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore at
-        rem fugit provident dolorum exercitationem, ducimus quisquam ratione
-        cumque reiciendis accusamus totam, veniam iusto numquam distinctio.
-        Dignissimos labore aspernatur pariatur?
-      </p>
-      <a href="#" className="btn">
+      <h4 className="front">Front-End</h4>
+      <br></br>
+      <ul>
+        <li><a className="linkProj" href="https://gabrieldevtrybe.github.io/StarWars/" target="_blank">Star Wars</a></li>
+        <li><a className="linkProj" href="https://gabrieldevtrybe.github.io/Front-End-Online-Store/" target="_blank">Online Store</a></li>
+        <li><a className="linkProj" href="https://gabrieldevtrybe.github.io/TrybeTunes/" target="_blank">Trybetunes</a></li>
+      </ul>
+      {/* <a href="#" className="btn">
         Ver Projetos
-      </a>
+      </a> */}
     </section>
   );
 };
