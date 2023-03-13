f8403ce6392db608d604f29bf6e21c72b1553e80
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2023-03-13 17:58:42 -0300

    Feat: Atualizando desccrição

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -6,15 +6,14 @@ const AboutContainer = () => {
       <h2>Sobre</h2>
       <p className="sobre-txt">
         Me chamo Gabriel Coelho tenho 27 anos, e atualmente sou
-        estudante da Trybe.
-        Já estando próximo a minha formação,
-        busco por oportunidades de iniciar minha carreira como desenvolvedor, e adquirir ainda mais conhecimento em tecnologias diversas.
+        desenvolvedor FullStack formado pela Trybe.
+        Busco por oportunidades de iniciar minha carreira como desenvolvedor, e adquirir ainda mais conhecimento em tecnologias diversas.
       </p>
       <br></br>
       <p className= "sobre-txt">
         O primeiro contato que tive com a tecnologia
         foi durante o curso da Trybe,
-        onde descobri meu interesse por resolução de problemas e programação. A partir daí venho constantemente buscar aprendizado.
+        onde descobri meu interesse por resolução de problemas e programação. A partir daí venho buscando constante aprendizado.
       </p>
       <br></br>
     </section>
