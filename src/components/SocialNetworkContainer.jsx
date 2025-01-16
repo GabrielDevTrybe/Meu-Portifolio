6863ad6ff896c7888066456413916264c67f8e84
Gabriel <gabrielcoelhodev1995@gmail.com>
2025-01-16 18:19:35 -0300

    atualizando link linkedin

--- src/components/SocialNetworkContainer.jsx
+++ src/components/SocialNetworkContainer.jsx
@@ -5,7 +5,7 @@ import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
 import "../styles/components/socialnetworkcontainer.sass";
 
 const socialNetworks = [
-  { name: "linkedin", icon: <FaLinkedinIn />, link: 'https://www.linkedin.com/in/gabriel-coelho-dev-junior/', },
+  { name: "linkedin", icon: <FaLinkedinIn />, link: 'https://www.linkedin.com/in/gabriel-coelho-dev-software/', },
   { name: "github", icon: <FaGithub />, link: 'https://github.com/GabrielDevTrybe'},
   { name: "instagram", icon: <FaInstagram />, link: 'https://www.instagram.com/gbcoelho1995/' },
 ];
