895f5c3c9a8e084afb36b38af0e247f4a3395cd5
Gabriel <gabrielcoelhodev1995@gmail.com>
2025-01-16 18:12:15 -0300

    arrumando o texto

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const AboutContainer = () => {
   return (
-    <section className="about-container">
-      <h2>Sobre</h2>
-      <p className="sobre-txt">
-      Desenvolvedor backend com 2 anos de experiência. 
-      Atuei por 1 ano e meio na FanBase, onde fui responsável por desenvolver integrações com foco em bancos de dados utilizando C#, MySQL e APIs REST. 
-      Atualmente, trabalho com Python, desenvolvendo automações empresariais e integrando funcionalidades de chatbots com WhatsApp. Tenho experiência em deploy de aplicações na AWS, especialmente utilizando o Lightsail. 
-      Sou apaixonado por otimizar processos e criar soluções escaláveis.
+    <section className="about-container" style={{ color: 'white' }}>
+      <h2 style={{ color: 'white' }}>Sobre</h2>
+      <p className="sobre-txt" style={{ color: 'white' }}>
+        Desenvolvedor backend com 2 anos de experiência. 
+        Atuei por 1 ano e meio na FanBase, onde fui responsável por desenvolver integrações com foco em bancos de dados utilizando C#, MySQL e APIs REST. 
+        Atualmente, trabalho com Python, desenvolvendo automações empresariais e integrando funcionalidades de chatbots com WhatsApp. Tenho experiência em deploy de aplicações na AWS, especialmente utilizando o Lightsail. 
+        Sou apaixonado por otimizar processos e criar soluções escaláveis.
       </p>
       <br></br>
     </section>
